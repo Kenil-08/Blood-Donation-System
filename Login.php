2b1e2d0f11fed384cb053702c283236b2650303d
kenil-08 <kenilkanani09@gmail.com>
2024-04-15 23:36:20 +0530

    Minor Change.

--- Login.php
+++ Login.php
@@ -1,3 +1,8 @@
+<?php
+    include "MyDb.php";
+    $obj = new MyDb();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,9 +35,7 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand text-dark" href="http://localhost/Blood-Donation-System/">Blood Donation System</a>
-            <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button> -->
+                <!--  -->
             <div class="collapse navbar-collapse justify-content-end " id="navbarNav">
                 <ul class="navbar-nav ">
                     <li class="nav-item">
@@ -77,8 +80,6 @@
 </html>
 
 <?php
-    include "MyDb.php";
-    $obj = new MyDb();
 
     if(isset($_POST['btnLogin']))
     {
